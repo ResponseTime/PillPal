b9b9ed92b4d21df92398a31ac4d207c56606998d
Looper <aayush.bhattacharjee2002@gmail.com>
2023-09-24 19:12:51 +0530

    worked on the web app and api

--- API/main.go
+++ API/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Nigga", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	var al = []album{}
+	for _, e := range albums {
+		if e.Title == "Nigga" {
+			al = append(al, e)
+		}
+	}
+	c.IndentedJSON(http.StatusOK, al)
+}
 func main() {
-	fmt.Println("api ")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
 }
--- WEB/PillPall-Web/src/App.jsx
+++ WEB/PillPall-Web/src/App.jsx
@@ -1,5 +1,49 @@
 import React from "react";
 
 export default function App() {
-  return <div></div>;
+  const data = [
+    { Day: "Monday", Time: "time23" },
+    { Day: "Tuesday", Time: "t76ime23" },
+    { Day: "Wednesday", Time: "tim76e" },
+    { Day: "Thursday", Time: "ti34me" },
+    { Day: "Friday", Time: "ti4me" },
+    { Day: "Saturday", Time: "ti7me" },
+    { Day: "Sunday", Time: "t87ime" },
+    { Day: "Monday", Time: "ti8me" },
+    { Day: "Friday", Time: "tim00e" },
+  ];
+  const groupedData = {};
+
+  data.forEach((item) => {
+    const day = item.Day;
+    if (!groupedData[day]) {
+      groupedData[day] = [];
+    }
+
+    if (
+      !groupedData[day].some((existingItem) => existingItem.Time === item.Time)
+    ) {
+      groupedData[day].push(item);
+    }
+  });
+
+  return (
+    <>
+      <div className="shower">
+        {Object.keys(groupedData).map((elem, index) => {
+          return (
+            <div>
+              <div className="day">{elem}</div>
+              <div className="act">
+                {groupedData[elem].map((item) => {
+                  return <div>{item.Time}</div>;
+                })}
+              </div>
+              ;
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
