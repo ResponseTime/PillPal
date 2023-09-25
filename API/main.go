2ecf1dea6619f575947faf461315be129ae5693e
Looper <aayush.bhattacharjee2002@gmail.com>
2023-09-25 12:29:01 +0530

    built scheduling and unscheduling routes

--- API/main.go
+++ API/main.go
@@ -8,44 +8,32 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
+var entries = make(map[string]cron.EntryID)
+var cronI *cron.Cron
 
-var albums = []album{
-	{ID: "1", Title: "Kaisen", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-
-	c.IndentedJSON(http.StatusOK, albums)
-}
 func setSchedule(c *gin.Context) {
 	item := c.Param("item")
-	cron := cron.New()
-	cron.AddFunc("@every 1m", func() { fmt.Println("reminder for ", item) })
-	cron.Start()
+
+	id, _ := cronI.AddFunc("@every 1m", func() { fmt.Println("reminder for ", item) })
+	entries[item] = id
+	cronI.Start()
 	st := struct {
 		Item string
 		Set  bool
 	}{Item: item, Set: true}
 	c.IndentedJSON(http.StatusOK, st)
 }
+
+func delSchedule(c *gin.Context) {
+	item := c.Param("item")
+	cronI.Remove(entries[item])
+	c.IndentedJSON(http.StatusOK, "done")
+}
 func main() {
+	cronI = cron.New()
 	router := gin.Default()
-	router.GET("api/albums", getAlbums)
 	router.GET("api/set/:item", setSchedule)
+	router.GET("api/del/:item", delSchedule)
 	router.Run("localhost:8080")
 
 }
-
-// func inspect(entry []cron.Entry) {
-// 	for _, entry := range entry {
-// 		fmt.Println(entry.Job)
-// 	}
-// }
